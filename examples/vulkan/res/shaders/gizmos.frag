bf169fd567cff6a654be10854c4f0c1b175cf570
Tim Roberts <ten3roberts@gmail.com>
2021-10-16 21:29:52 +0200

    Make gizmos fade behind objects

--- examples/vulkan/res/shaders/gizmos.frag
+++ examples/vulkan/res/shaders/gizmos.frag
@@ -6,9 +6,18 @@ layout(location = 1) in vec4 fragColor;
 
 layout(location = 0) out vec4 outColor;
 
-void main() {
-  if (length(fragModelPosition) > 1)
-      discard;
+layout (input_attachment_index = 0, binding = 0, set = 1) uniform subpassInput depthInput;
 
-  outColor = fragColor;
+void main() {
+  float depth = subpassLoad(depthInput).x;
+  float currentDepth = gl_FragCoord.z;
+  float opacity = 1.0;
+
+  if (currentDepth > depth)
+  opacity = 0.2;
+
+  if (length(fragModelPosition) > 1)
+  discard;
+
+  outColor = fragColor * vec4(1,1,1,opacity);
 }
---
+++ ivy-collision/src/tree.rs
@@ -0,0 +1,318 @@
+use arrayvec::{Array, ArrayVec};
+use hecs::{Entity, World};
+use ivy_core::{Gizmo, GizmoKind, Gizmos, Position, Scale};
+use slotmap::{new_key_type, SlotMap};
+use ultraviolet::{Vec3, Vec4};
+
+use crate::{Collider, CollisionPrimitive, Sphere};
+
+/// Marker for where the object is in the tree
+struct TreeMarker {
+    index: NodeIndex,
+}
+
+new_key_type!(
+    pub struct NodeIndex;
+);
+
+type Nodes<C> = SlotMap<NodeIndex, Node<C>>;
+
+pub struct CollisionTree<C: Array<Item = Object>> {
+    nodes: SlotMap<NodeIndex, Node<C>>,
+    root: NodeIndex,
+}
+
+impl<C: Array<Item = Object>> CollisionTree<C> {
+    pub fn new(half_extents: Vec3) -> Self {
+        let mut nodes = SlotMap::with_key();
+        let root = nodes.insert(Node::new(Vec3::zero(), half_extents));
+        Self { nodes, root }
+    }
+
+    pub fn contains(&self, object: &Object) -> bool {
+        self.nodes[self.root].contains(object)
+    }
+
+    /// Get a reference to the collision tree's nodes.
+    pub fn nodes(&self) -> &SlotMap<NodeIndex, Node<C>> {
+        &self.nodes
+    }
+
+    /// Get a mutable reference to the collision tree's nodes.
+    pub fn nodes_mut(&mut self) -> &mut SlotMap<NodeIndex, Node<C>> {
+        &mut self.nodes
+    }
+
+    pub fn register(&mut self, world: &mut World) {
+        let insterted = world
+            .query::<(&Collider, &Position, &Scale)>()
+            .without::<TreeMarker>()
+            .iter()
+            .map(|(e, (collider, position, scale))| {
+                Object::new(
+                    e,
+                    Sphere::new(collider.max_radius() * scale.component_max()),
+                    **position,
+                )
+            })
+            .collect::<Vec<_>>();
+
+        insterted.into_iter().for_each(|object| {
+            let entity = object.entity;
+            let index = self.insert(object);
+            world.insert_one(entity, TreeMarker { index }).unwrap();
+        })
+    }
+
+    /// Inserts an object into the tree and returns where it was placed. May
+    /// split the tree
+    pub fn insert(&mut self, object: Object) -> NodeIndex {
+        Node::insert(self.root, &mut self.nodes, object)
+    }
+
+    pub fn draw_gizmos(&self, gizmos: &mut Gizmos) {
+        Node::draw_gizmos(self.root, &self.nodes, 1, gizmos);
+    }
+}
+
+pub struct Node<C: Array<Item = Object>> {
+    objects: ArrayVec<C>,
+    origin: Vec3,
+    half_extents: Vec3,
+    children: Option<[NodeIndex; 2]>,
+}
+
+impl<C: Array<Item = Object>> Node<C> {
+    pub fn new(origin: Vec3, half_extents: Vec3) -> Self {
+        Self {
+            objects: ArrayVec::default(),
+            origin,
+            half_extents,
+            children: None,
+        }
+    }
+
+    /// Inserts into node. Does not check if it is fully contained or if already
+    /// in node.
+    pub fn insert(current: NodeIndex, nodes: &mut Nodes<C>, object: Object) -> NodeIndex {
+        // Check if any child can contain it
+        if let Some(child) = nodes[current]
+            .children
+            .iter()
+            .flatten()
+            .find(|child| nodes[**child].contains(&object))
+        {
+            Self::insert(*child, nodes, object)
+        } else {
+            let node = &mut nodes[current];
+            if let Err(err) = node.objects.try_push(object) {
+                Self::split(current, nodes);
+                Self::insert(current, nodes, err.element())
+            } else {
+                current
+            }
+        }
+    }
+
+    /// Splits the node in half
+    pub fn split(current: NodeIndex, nodes: &mut Nodes<C>) {
+        let mut center = Vec3::zero();
+        let mut max = Vec3::zero();
+        let mut min = Vec3::zero();
+
+        let node = &mut nodes[current];
+        node.objects.iter().for_each(|val| {
+            center += val.origin;
+            max = max.max_by_component(val.origin);
+            min = min.min_by_component(val.origin);
+        });
+
+        let len = node.objects.len();
+        let center = center * (1.0 / len as f32);
+
+        let width = (max - min).abs();
+
+        let max = max_axis(width);
+        dbg!(max);
+
+        let off = node.half_extents * max * 0.5;
+
+        let extents = node.half_extents - off;
+        let a_origin = node.origin - off;
+        let b_origin = node.origin + off;
+
+        let _rel_center = center - node.origin;
+
+        let a = nodes.insert(Node::new(a_origin, extents));
+        let b = nodes.insert(Node::new(b_origin, extents));
+
+        nodes[current].children = Some([a, b]);
+    }
+
+    /// Returns true if the object bounded by a sphere fits in the node.
+    pub fn contains(&self, object: &Object) -> bool {
+        object.origin.x + object.bound.radius < self.origin.x + self.half_extents.x
+            && object.origin.x - object.bound.radius > self.origin.x - self.half_extents.x
+            && object.origin.y + object.bound.radius < self.origin.y + self.half_extents.y
+            && object.origin.y - object.bound.radius > self.origin.y - self.half_extents.y
+            && object.origin.z + object.bound.radius < self.origin.z + self.half_extents.z
+            && object.origin.z - object.bound.radius > self.origin.z - self.half_extents.z
+    }
+
+    pub fn draw_gizmos(current: NodeIndex, nodes: &Nodes<C>, depth: usize, gizmos: &mut Gizmos) {
+        let node = &nodes[current];
+
+        gizmos.push(Gizmo::new(
+            node.origin,
+            Vec3::zero(),
+            0.2,
+            Vec4::new(1.0, 0.0, 1.0, 1.0) * (1.0 / depth as f32),
+            GizmoKind::Sphere,
+        ));
+
+        gizmos.push(Gizmo::new(
+            node.origin - node.half_extents,
+            Vec3::unit_x() * node.half_extents * 2.0,
+            0.1,
+            Vec4::new(1.0, 0.0, 1.0, 1.0),
+            GizmoKind::Line,
+        ));
+
+        gizmos.push(Gizmo::new(
+            node.origin - node.half_extents,
+            Vec3::unit_z() * node.half_extents * 2.0,
+            0.1,
+            Vec4::new(1.0, 0.0, 1.0, 1.0),
+            GizmoKind::Line,
+        ));
+
+        gizmos.push(Gizmo::new(
+            node.origin - node.half_extents,
+            Vec3::unit_y() * node.half_extents * 2.0,
+            0.1,
+            Vec4::new(1.0, 0.0, 1.0, 1.0),
+            GizmoKind::Line,
+        ));
+
+        gizmos.push(Gizmo::new(
+            node.origin + node.half_extents,
+            -Vec3::unit_x() * node.half_extents * 2.0,
+            0.1,
+            Vec4::new(1.0, 0.0, 1.0, 1.0),
+            GizmoKind::Line,
+        ));
+
+        gizmos.push(Gizmo::new(
+            node.origin + node.half_extents,
+            -Vec3::unit_z() * node.half_extents * 2.0,
+            0.1,
+            Vec4::new(1.0, 0.0, 1.0, 1.0),
+            GizmoKind::Line,
+        ));
+
+        gizmos.push(Gizmo::new(
+            node.origin + node.half_extents,
+            -Vec3::unit_y() * node.half_extents * 2.0,
+            0.1,
+            Vec4::new(1.0, 0.0, 1.0, 1.0),
+            GizmoKind::Line,
+        ));
+
+        gizmos.push(Gizmo::new(
+            node.origin - node.half_extents * Vec3::new(1.0, -1.0, 1.0),
+            Vec3::unit_x() * node.half_extents * 2.0,
+            0.1,
+            Vec4::new(1.0, 0.0, 1.0, 1.0),
+            GizmoKind::Line,
+        ));
+
+        gizmos.push(Gizmo::new(
+            node.origin + node.half_extents * Vec3::new(-1.0, -1.0, 1.0),
+            Vec3::unit_y() * node.half_extents * 2.0,
+            0.1,
+            Vec4::new(1.0, 1.0, 1.0, 1.0),
+            GizmoKind::Line,
+        ));
+
+        gizmos.push(Gizmo::new(
+            node.origin + node.half_extents * Vec3::new(-1.0, -1.0, 1.0),
+            Vec3::unit_x() * node.half_extents * 2.0,
+            0.1,
+            Vec4::new(1.0, 1.0, 1.0, 1.0),
+            GizmoKind::Line,
+        ));
+
+        gizmos.push(Gizmo::new(
+            node.origin + node.half_extents * Vec3::new(-1.0, -1.0, 1.0),
+            Vec3::unit_x() * node.half_extents * 2.0,
+            0.1,
+            Vec4::new(1.0, 1.0, 1.0, 1.0),
+            GizmoKind::Line,
+        ));
+
+        gizmos.push(Gizmo::new(
+            node.origin + node.half_extents * Vec3::new(1.0, -1.0, -1.0),
+            Vec3::unit_z() * node.half_extents * 2.0,
+            0.1,
+            Vec4::new(1.0, 1.0, 1.0, 1.0),
+            GizmoKind::Line,
+        ));
+
+        gizmos.push(Gizmo::new(
+            node.origin + node.half_extents * Vec3::new(-1.0, 1.0, -1.0),
+            Vec3::unit_z() * node.half_extents * 2.0,
+            0.1,
+            Vec4::new(1.0, 1.0, 1.0, 1.0),
+            GizmoKind::Line,
+        ));
+
+        gizmos.push(Gizmo::new(
+            node.origin + node.half_extents * Vec3::new(1.0, -1.0, -1.0),
+            Vec3::unit_y() * node.half_extents * 2.0,
+            0.1,
+            Vec4::new(1.0, 1.0, 1.0, 1.0),
+            GizmoKind::Line,
+        ));
+
+        node.children
+            .iter()
+            .flatten()
+            .for_each(|val| Self::draw_gizmos(*val, nodes, depth + 1, gizmos))
+    }
+}
+
+/// Represents an entity with extra collider information
+pub struct Object {
+    entity: Entity,
+    bound: Sphere,
+    origin: Vec3,
+}
+
+impl Object {
+    pub fn new(entity: Entity, bound: Sphere, origin: Vec3) -> Self {
+        Self {
+            entity,
+            bound,
+            origin,
+        }
+    }
+
+    /// Get a reference to the object's entity.
+    pub fn entity(&self) -> Entity {
+        self.entity
+    }
+}
+
+fn max_axis(val: Vec3) -> Vec3 {
+    if val.x > val.y {
+        if val.x > val.z {
+            Vec3::new(1.0, 0.0, 0.0)
+        } else {
+            Vec3::new(0.0, 0.0, 1.0)
+        }
+    } else if val.y > val.z {
+        Vec3::new(0.0, 1.0, 0.0)
+    } else {
+        Vec3::new(0.0, 0.0, 1.0)
+    }
+}
