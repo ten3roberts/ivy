9f0af267fbc3855cbe60c018b20337c4b45f0aba
Tei Roberts <ten3roberts@gmail.com>
2022-02-18 13:06:39 +0100

    Allow multifloor elevators

--- ivy-graphics/src/shaders.rs
+++ ivy-graphics/src/shaders.rs
@@ -1,43 +1,61 @@
 use ivy_vulkan::ShaderModuleInfo;
 
 /// Renders a fullscreen quad, has no input data
-pub const FULLSCREEN_SHADER: ShaderModuleInfo =
-    ShaderModuleInfo::from_const_bytes(include_bytes!(concat!(env!("OUT_DIR"), "/shaders/fullscreen.vert.spv")));
+pub const FULLSCREEN_SHADER: ShaderModuleInfo = ShaderModuleInfo::from_const_bytes(include_bytes!(
+    concat!(env!("OUT_DIR"), "/shaders/fullscreen.vert.spv")
+));
 
-pub const DEFAULT_VERTEX_SHADER: ShaderModuleInfo =
-    ShaderModuleInfo::from_const_bytes(include_bytes!(concat!(env!("OUT_DIR"), "/shaders/default.vert.spv")));
+pub const DEFAULT_VERTEX_SHADER: ShaderModuleInfo = ShaderModuleInfo::from_const_bytes(
+    include_bytes!(concat!(env!("OUT_DIR"), "/shaders/default.vert.spv")),
+);
 
-pub const FORWARD_VERTEX_SHADER: ShaderModuleInfo =
-    ShaderModuleInfo::from_const_bytes(include_bytes!(concat!(env!("OUT_DIR"), "/shaders/forward.vert.spv")));
+pub const FORWARD_VERTEX_SHADER: ShaderModuleInfo = ShaderModuleInfo::from_const_bytes(
+    include_bytes!(concat!(env!("OUT_DIR"), "/shaders/forward.vert.spv")),
+);
 
-pub const FORWARD_FRAGMENT_SHADER: ShaderModuleInfo =
-    ShaderModuleInfo::from_const_bytes(include_bytes!(concat!(env!("OUT_DIR"), "/shaders/forward.frag.spv")));
+pub const FORWARD_FRAGMENT_SHADER: ShaderModuleInfo = ShaderModuleInfo::from_const_bytes(
+    include_bytes!(concat!(env!("OUT_DIR"), "/shaders/forward.frag.spv")),
+);
 
-pub const DEFAULT_FRAGMENT_SHADER: ShaderModuleInfo =
-    ShaderModuleInfo::from_const_bytes(include_bytes!(concat!(env!("OUT_DIR"), "/shaders/default.frag.spv")));
+pub const DEFAULT_FRAGMENT_SHADER: ShaderModuleInfo = ShaderModuleInfo::from_const_bytes(
+    include_bytes!(concat!(env!("OUT_DIR"), "/shaders/default.frag.spv")),
+);
 
-pub const SKINNED_VERTEX_SHADER: ShaderModuleInfo =
-    ShaderModuleInfo::from_const_bytes(include_bytes!(concat!(env!("OUT_DIR"), "/shaders/skinned.vert.spv")));
+pub const TRIPLANAR_FRAGMENT_SHADER: ShaderModuleInfo = ShaderModuleInfo::from_const_bytes(
+    include_bytes!(concat!(env!("OUT_DIR"), "/shaders/triplanar.frag.spv")),
+);
 
-pub const GIZMO_VERTEX_SHADER: ShaderModuleInfo =
-    ShaderModuleInfo::from_const_bytes(include_bytes!(concat!(env!("OUT_DIR"), "/shaders/gizmos.vert.spv")));
-pub const GIZMO_FRAGMENT_SHADER: ShaderModuleInfo =
-    ShaderModuleInfo::from_const_bytes(include_bytes!(concat!(env!("OUT_DIR"), "/shaders/gizmos.frag.spv")));
+pub const SKINNED_VERTEX_SHADER: ShaderModuleInfo = ShaderModuleInfo::from_const_bytes(
+    include_bytes!(concat!(env!("OUT_DIR"), "/shaders/skinned.vert.spv")),
+);
+
+pub const GIZMO_VERTEX_SHADER: ShaderModuleInfo = ShaderModuleInfo::from_const_bytes(
+    include_bytes!(concat!(env!("OUT_DIR"), "/shaders/gizmos.vert.spv")),
+);
+
+pub const GIZMO_FRAGMENT_SHADER: ShaderModuleInfo = ShaderModuleInfo::from_const_bytes(
+    include_bytes!(concat!(env!("OUT_DIR"), "/shaders/gizmos.frag.spv")),
+);
 
 // UI
-pub const IMAGE_FRAGMENT_SHADER: ShaderModuleInfo =
-    ShaderModuleInfo::from_const_bytes(include_bytes!(concat!(env!("OUT_DIR"), "/shaders/ui/image.frag.spv")));
+pub const IMAGE_FRAGMENT_SHADER: ShaderModuleInfo = ShaderModuleInfo::from_const_bytes(
+    include_bytes!(concat!(env!("OUT_DIR"), "/shaders/ui/image.frag.spv")),
+);
 
-pub const TEXT_FRAGMENT_SHADER: ShaderModuleInfo =
-    ShaderModuleInfo::from_const_bytes(include_bytes!(concat!(env!("OUT_DIR"), "/shaders/ui/text.frag.spv")));
+pub const TEXT_FRAGMENT_SHADER: ShaderModuleInfo = ShaderModuleInfo::from_const_bytes(
+    include_bytes!(concat!(env!("OUT_DIR"), "/shaders/ui/text.frag.spv")),
+);
 
-pub const IMAGE_VERTEX_SHADER: ShaderModuleInfo =
-    ShaderModuleInfo::from_const_bytes(include_bytes!(concat!(env!("OUT_DIR"), "/shaders/ui/image.vert.spv")));
+pub const IMAGE_VERTEX_SHADER: ShaderModuleInfo = ShaderModuleInfo::from_const_bytes(
+    include_bytes!(concat!(env!("OUT_DIR"), "/shaders/ui/image.vert.spv")),
+);
 
-pub const TEXT_VERTEX_SHADER: ShaderModuleInfo =
-    ShaderModuleInfo::from_const_bytes(include_bytes!(concat!(env!("OUT_DIR"), "/shaders/ui/text.vert.spv")));
+pub const TEXT_VERTEX_SHADER: ShaderModuleInfo = ShaderModuleInfo::from_const_bytes(
+    include_bytes!(concat!(env!("OUT_DIR"), "/shaders/ui/text.vert.spv")),
+);
 
 // Pbr
 /// Generic PBR lighting deferred lighting shader
-pub const PBR_SHADER: ShaderModuleInfo =
-    ShaderModuleInfo::from_const_bytes(include_bytes!(concat!(env!("OUT_DIR"), "/shaders/pbr_lighting.frag.spv")));
+pub const PBR_SHADER: ShaderModuleInfo = ShaderModuleInfo::from_const_bytes(include_bytes!(
+    concat!(env!("OUT_DIR"), "/shaders/pbr_lighting.frag.spv")
+));
